76f12674df21537b058617e1e8e8791fa09b0ab6
sda-dasa <saltykovadasa15@gmail.com>
2026-06-15 22:57:09 +0300

    Update database.py

--- src/database.py
+++ src/database.py
@@ -6,6 +6,7 @@ engine = create_async_engine(config.DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
+
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
